fbc643bfc45705672648911910b99fb7ac9e145f
Robert Virany <robertvirany@gmail.com>
2025-09-19 22:41:17 -0600

    simple tool call achieved

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
 use std::{fs, path::PathBuf};
-use uuid::Uuid;
+//use uuid::Uuid;
 use futures_util::StreamExt;
 
 #[derive(Deserialize)]
@@ -26,11 +26,15 @@ async fn main() -> anyhow::Result<()> {
     let access_token = &auth.tokens.access_token;
     let account_id = &auth.tokens.account_id;
 
-    let conv_id = Uuid::new_v4().to_string();
+    let conv_id = String::from("redacted_for_git");
+    
+    const BASE_PROMPT: &str = include_str!("prompt.md");
+    //let prompt_cache_key = conv_id.clone();
+    //let instructions = format!("{BASE_PROMPT}\n\nYou are a terse assistant. Who was the first president of the USA.");
 
     let body = serde_json::json!({
         "model": "gpt-5", 
-        "instructions": "you are a terse assistant.",
+        "instructions": BASE_PROMPT,
         "input": [
             {
                 "type":"message",
@@ -43,34 +47,36 @@ async fn main() -> anyhow::Result<()> {
         "tools": [],
         "tool_choice": "auto",
         "parallel_tool_calls": false,
-        "reasoning": serde_json::Value::Null,
+        "reasoning": {"summary":"auto"},
         "store": false,
         "stream": true,
-        "include": [],
+        "include": ["reasoning.encrypted_content"],
         "prompt_cache_key": conv_id,
-        "text": serde_json::Value::Null
     });
 
     let client = reqwest::Client::new();
     let res = client
         .post("https://chatgpt.com/backend-api/codex/responses")
         .bearer_auth(access_token)
-        .header("version", //VERSION//)
+        //.header("version", "0.34.0")
         .header("openai-beta", "responses=experimental")
         .header("conversation_id", &conv_id)
         .header("session_id", &conv_id)
         .header("accept", "text/event-stream")
         .header("content-type", "application/json")
         .header("chatgpt-account-id", account_id)
-        .header("user-agent", codex_cli_rs///VERSION// (Ubuntu 24.4.0; x86_64) WindowsTerminal)
-        .header("originator", "codex_cli_rs")
-        .header("host", "chatgpt.com")
-        .header("content-length", //CONTENT-LENGTH//)
+        //.header("user-agent", "codex_cli_rs/0.34.0 (Ubuntu 24.4.0; x86_64) WindowsTerminal")
+        //.header("originator", "codex_cli_rs")
+        //.header("host", "chatgpt.com")
+        //.header("content-length", //CONTENT-LENGTH//)
         .json(&body)
         .send()
         .await?;
 
-    println!("status: {}", res.status());
+    if !res.status().is_success() {
+        eprintln!("{}: {}", res.status(), res.text().await?);
+        return Ok(());
+    }
     let mut stream = res.bytes_stream();
 
     while let Some(chunk) = stream.next().await {
